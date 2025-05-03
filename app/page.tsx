f92b365daff6e95353d7e5fb980051054389ed09
nottnt <tanatat19@outlook.com>
2025-05-04 01:11:20 +0700

    feat: adjust filter and search condition

--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,72 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import PokemonCard from "@/components/PokemonCard";
 import { getOfficialArtwork } from "@/utils";
-import { fetchPokemonByName } from "@/lib/fetchPokemons";
 import { usePokemons } from "@/hooks/usePokemons";
+import { NamedAPIResource } from "pokenode-ts";
+import { DATA_PER_PAGE } from "@/constants";
 
 export default function Home() {
-  const { data, isLoading } = usePokemons();
+  const { data: allPokemons = [], isLoading } = usePokemons();
   const [searchTerm, setSearchTerm] = useState("");
-  const [searchedPokemon, setSearchedPokemon] = useState<null | {
-    name: string;
-    imageUrl: string;
-  }>(null);
-  const [error, setError] = useState("");
-
-  const allPokemons = data || [];
+  const [filteredPokemons, setFilteredPokemons] =
+    useState<NamedAPIResource[]>(allPokemons);
+  const [displayPokemons, setDisplayPokemons] =
+    useState<NamedAPIResource[]>(allPokemons);
+  const [page, setPage] = useState(1);
 
   const handleSearch = async (e: React.FormEvent) => {
     e.preventDefault();
-    if (!searchTerm.trim()) return;
-
-    try {
-      const pokemon = await fetchPokemonByName(searchTerm);
-
-      setSearchedPokemon({
-        name: pokemon.name,
-        imageUrl: getOfficialArtwork(`${pokemon.id}`),
-      });
-      setError("");
-    } catch (err) {
-      console.error(err);
-      setError("Pokémon not found!");
-      setSearchedPokemon(null);
+    if (!searchTerm.trim()) initPokemons();
+    else {
+      const filteredPokemons = allPokemons.filter((pokemon) =>
+        pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      setFilteredPokemons(filteredPokemons);
+      setDisplayPokemons(slicePokemons(filteredPokemons));
     }
+    setPage(1);
   };
 
-  const filteredPokemons = allPokemons.filter((pokemon) =>
-    pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+  const slicePokemons = (
+    pokemons: NamedAPIResource[],
+    start: number = 0,
+    end: number = DATA_PER_PAGE
+  ) => {
+    const slicedPokemons = pokemons.slice(start, end);
+    return slicedPokemons;
+  };
+
+  const handleNextPage = (page: number) => {
+    const nextPokemons = slicePokemons(
+      filteredPokemons,
+      (page - 1) * DATA_PER_PAGE,
+      page * DATA_PER_PAGE
+    );
+    console.log("nextPokemons", nextPokemons);
+    setDisplayPokemons((prev) => [...prev, ...nextPokemons]);
+  };
+
+  const initPokemons = () => {
+    const initialPokemons = slicePokemons(allPokemons);
+    setDisplayPokemons(initialPokemons);
+    setFilteredPokemons(allPokemons);
+  };
+
+  useEffect(() => {
+    if (allPokemons.length > 0) {
+      initPokemons();
+    }
+  }, [allPokemons]);
+
+  useEffect(() => {
+    handleNextPage(page);
+  }, [page]);
+
+  const hasNextPage = useMemo(() => {
+    return displayPokemons.length < filteredPokemons.length;
+  }, [filteredPokemons, displayPokemons]);
 
   return (
     <main className="p-8">
@@ -59,44 +87,30 @@ export default function Home() {
         </button>
       </form>
 
-      {error && <div className="text-red-500 text-center mb-4">{error}</div>}
       {isLoading ? (
         <div className="text-center">Loading...</div>
       ) : (
         <>
-          {/* If searched Pokémon found, show it */}
-          {searchedPokemon ? (
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+          {/* Else show normal Pokémon list */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+            {displayPokemons.map((pokemon, index) => (
               <PokemonCard
-                name={searchedPokemon.name}
-                imageUrl={searchedPokemon.imageUrl}
+                key={`${pokemon.name}-${index}`}
+                name={pokemon.name}
+                imageUrl={getOfficialArtwork(pokemon.url)}
               />
-            </div>
-          ) : (
-            <>
-              {/* Else show normal Pokémon list */}
-              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-                {filteredPokemons.map((pokemon) => (
-                  <PokemonCard
-                    key={pokemon.name}
-                    name={pokemon.name}
-                    imageUrl={getOfficialArtwork(pokemon.url)}
-                  />
-                ))}
-              </div>
+            ))}
+          </div>
 
-              {/* {hasNextPage && (
-                <div className="flex justify-center mt-8">
-                  <button
-                    onClick={() => fetchNextPage()}
-                    disabled={isFetchingNextPage}
-                    className="bg-green-600 text-white px-6 py-3 rounded hover:bg-green-700 disabled:opacity-50"
-                  >
-                    {isFetchingNextPage ? "Loading more..." : "Load More"}
-                  </button>
-                </div>
-              )} */}
-            </>
+          {hasNextPage && (
+            <div className="flex justify-center mt-8">
+              <button
+                onClick={() => setPage((prev) => prev + 1)}
+                className="bg-green-600 text-white px-6 py-3 rounded hover:bg-green-700 disabled:opacity-50"
+              >
+                Load More
+              </button>
+            </div>
           )}
         </>
       )}
