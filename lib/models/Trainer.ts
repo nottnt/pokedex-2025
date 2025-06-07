8e51ca488d108a83b69750d3735a199499d2fe3c
nottnt <tanatat19@outlook.com>
2025-06-08 00:55:13 +0700

    feat: ref user with trainer by user id

--- lib/models/Trainer.ts
+++ lib/models/Trainer.ts
@@ -2,11 +2,14 @@ import { Schema, model, models, Document, Model } from "mongoose";
 import { TrainerFormData } from "../validation/trainer";
 
 // 1. Define the interface for TypeScript
-export interface ITrainer extends Document, TrainerFormData {}
+export interface ITrainer extends Document, TrainerFormData {
+  userId: string;
+}
 
 // 2. Create the schema
 const TrainerSchema = new Schema<ITrainer>(
   {
+    userId: { type: String, required: true, unique: true },
     name: { type: String, required: true },
     age: { type: Number, required: true },
     region: { type: String, required: true },
@@ -21,3 +24,5 @@ const TrainerSchema = new Schema<ITrainer>(
 // 3. Create and export the model
 export const Trainer: Model<ITrainer> =
   models.Trainer || model<ITrainer>("Trainer", TrainerSchema);
+
+export default Trainer;
